f333652a2954a0e5e5284ebd532ec72eb15dcabf
Nickr <romero.nicholejohn@gmail.com>
2025-10-03 05:15:27 +0800

    Add react aria components; Non-linear Slider demo (#1)

--- src/routes/non-linear-slider/index.tsx
+++ src/routes/non-linear-slider/index.tsx
@@ -1,9 +1,47 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Label,
+  Slider,
+  SliderOutput,
+  SliderThumb,
+  SliderTrack,
+} from 'react-aria-components';
 
 export const Route = createFileRoute('/non-linear-slider/')({
-  component: About,
+  component: NonLinearSlider,
 });
 
-function About() {
-  return <div>Non-Linear Slider</div>;
+function NonLinearSlider() {
+  const sliderRef = useRef<HTMLInputElement>(null);
+
+  const steps = [10, 50, 100, 250, 500, 1000];
+  const [value, setValue] = useState(0);
+
+  const formattedDistance = useMemo(() => `${steps[value]}km`, [value]);
+
+  useEffect(() => {
+    if (!sliderRef.current) return;
+
+    sliderRef.current.setAttribute('aria-valuetext', formattedDistance);
+  }, [formattedDistance]);
+
+  return (
+    <div>
+      <Slider
+        className="slider"
+        maxValue={steps.length - 1}
+        value={value}
+        onChange={setValue}
+      >
+        <Label>Distance</Label>
+        <SliderOutput className="slider-output">
+          {formattedDistance}
+        </SliderOutput>
+        <SliderTrack className="slider-track">
+          <SliderThumb className="slider-thumb" inputRef={sliderRef} />
+        </SliderTrack>
+      </Slider>
+    </div>
+  );
 }
