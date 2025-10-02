dd51810c814ca347b7ec0531a160edfc558cc4a4
boringdeveloper <romero.nicholejohn@gmail.com>
2025-10-03 03:22:44 +0800

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>React Aria Components Adventure</h1>
+      <p>
+        Some of the experiences I had while using React Aria Components to build
+        an accessible and user-friendly application.
+      </p>
+    </div>
+  );
+};
+
+export default App;
